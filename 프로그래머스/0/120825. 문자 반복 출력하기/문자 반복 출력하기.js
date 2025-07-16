d2c50836c3abf56cd0337ad8a62ab9071fe97c24
kim ha young <158129874+zerozeroha@users.noreply.github.com>
2025-07-16 11:03:15 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -2,5 +2,5 @@ function solution(my_string, n) {
     
     // str.repeat(n): str(문자열)을 n번 반복한 새로운 문자열을 반환
     
-    return my_string.split("").map(str => str.repeat(n)).join("")
+    return my_string.split("").map(str=>str.repeat(n)).join("");
 }

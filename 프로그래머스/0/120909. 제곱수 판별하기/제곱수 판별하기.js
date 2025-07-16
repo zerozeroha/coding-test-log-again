bcd5fe30e313a806454785d39151caab1d228963
kim ha young <158129874+zerozeroha@users.noreply.github.com>
2025-07-16 11:07:09 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,8 +1,11 @@
 function solution(n) {
+    
+    // Number.isInteger(x): x가 정수이면 true를 반환
+    // Math.sqrt: 전달한 숫자의 제곱근을 반환하는 함수
  
-    if(Number.isInteger(Math.sqrt(n))){
-        return 1
-    }else{
-        return 2
-    }
+   if(Number.isInteger(Math.sqrt(n))){
+       return 1
+   }else{
+       return 2
+   }
 }

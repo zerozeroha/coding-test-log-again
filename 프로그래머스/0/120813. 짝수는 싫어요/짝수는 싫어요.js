93233c93766fd68b29af7f01383fdbfc5895f341
kim ha young <158129874+zerozeroha@users.noreply.github.com>
2025-07-16 11:27:35 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,12 @@
+function solution(n) {
+   
+    let oddNum = []
+    
+    for(let i=0; i<=n; i++){
+        if(i % 2 === 1){
+            oddNum.push(i)
+        }
+    } return oddNum
+    
+    
+}
